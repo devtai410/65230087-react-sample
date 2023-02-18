5ba8bb8f0d1618d52109a022f342f6bae4ddb17b
devtai410 <waiyawootlove@gmail.com>
2023-02-18 15:40:23 +0700

    setup gh-pages

--- src/report.jsx
+++ src/report.jsx
@@ -7,6 +7,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
+import Image from 'react-bootstrap/Image';
 import axios from "axios";
 import Common from "./common";
 const BASE_URL = Common.API_URL;
@@ -65,8 +66,12 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div style={{color: "#ffffff",fontSize: "19px"}}>นาย วัยวุฒิ ชุมเมืองปัก 65230087</div>
-           fsdfsd
+            <div style={{color: "#ffffff",fontSize: "19px"}}>
+              <Image src="https://scontent.fbkk15-1.fna.fbcdn.net/v/t39.30808-6/298916475_525632722700791_6657034722643333909_n.png?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=p1aOvqB-BFsAX8Enki6&_nc_ht=scontent.fbkk15-1.fna&oh=00_AfD-36ZLdtuzMXED2DJji3vsYz3_gFmiyhuRKAutZjVRQg&oe=63F5ECC8" 
+              roundedCircle  style={{width: "50px",height: "50px;"}}/>
+              
+             {" "}65230087 นาย วัยวุฒิ ชุมเมืองปัก </div>
+         
           </Container>
         </Navbar>
         <Container>
@@ -98,7 +103,7 @@ export default class report extends Component {
           <div style={{ paddingTop: "15px" }}>
             <Card>
               <Card.Body>
-                <Table striped bordered hover>
+                <Table striped bordered hover class="table table-sm">
                   <thead>
                     <tr>
                       <th>#</th>
